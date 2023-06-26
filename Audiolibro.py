a582f12903725a85877290ea6fe6d39c0c007915
Francisco Hernández Castillo <48812779+frahercas@users.noreply.github.com>
2023-06-26 17:24:20 -0600

    Update Audiolibro.py
    Código que nos permite leer un archivo PDF, leerlo y convertir su contenido en un audio, lo que nos permite obtener algo semejante a un audiolibro.

--- Audiolibro.py
+++ Audiolibro.py
@@ -1,7 +1,7 @@
 import PyPDF2
 import pyttsx3
 
-pdf_reader = PyPDF2.PdfReader(open(r'C:\Users\herca\OneDrive\Documentos\PDFs General\George1984.pdf', 'rb'))
+pdf_reader = PyPDF2.PdfReader(open(r'XXXX.pdf', 'rb')) # En lugar de "XXXX" escribe el path donde se ubica el archivo PDF que quieres abrir seguido de la extención .pdf
 
 speaker = pyttsx3.init()
 engine = pyttsx3.init()
@@ -14,7 +14,7 @@ for page_num in range(len(pdf_reader.pages)):
     speaker.runAndWait()
     
     # Guardar el texto en un archivo de audio
-    audio_file = fr'C:\Users\herca\Music\Audiolibros\page_{page_num}.mp3'
+    audio_file = fr'XXXX.mp3' #Eb lugar de "XXXX" escribe el path donde deseas guardar el audio generaado seguido de la extención .mp3
     engine.save_to_file(text, audio_file)
     engine.runAndWait()
 
